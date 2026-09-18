ffe9dde15c6a33d4392981603978cb578719c5d3
Gliimu <269347753+Gliimu@users.noreply.github.com>
2026-09-18 10:30:11 +0100

    Update print.js

--- pos-system/frontend/js/utils/print.js
+++ pos-system/frontend/js/utils/print.js
@@ -16,7 +16,7 @@ export function printReceipt(cart, total, attendantName) {
     const receiptHTML = `
         <div class="receipt-header">
             <h2>RamsyPOS</h2>
-            <p>Maitama 2, Berger Qwarry Road, FCT-Abuja</p>
+            <p>123 Maitama 2, Berger Qwarry Road, FCT-Abuja</p>
             <p>Tel: 080123456789</p>
             <p>${date}</p>
         </div>
