062a45859475d864a7b248beff8f73b36e610676
Gliimu <269347753+Gliimu@users.noreply.github.com>
2026-09-19 11:38:22 +0100

    Modify receipt header with new location and contact
    Updated address and contact details in the receipt header.

--- pos-system/frontend/js/utils/print.js
+++ pos-system/frontend/js/utils/print.js
@@ -18,8 +18,8 @@ export function printReceipt(cart, total, attendantName, modeOfPayment) {
             <img src="assets/logo.png" alt="RamsyPOS">
             <h2 style="margin: 0; font-size: 18px;">Ramsy Paradise</h2>
             <p style="margin: 2px 0;">Hotel & Suites</p>
-            <p style="margin: 2px 0;">123 Business Street, Lagos</p>
-            <p style="margin: 2px 0;">Tel: 080XXXXXXX</p>
+            <p style="margin: 2px 0;">Maitama 2, Berger Quarry Road, Mpape, Abuja</p>
+            <p style="margin: 2px 0;">Tel: +2349 06155 5082</p>
             <p style="margin: 5px 0 0 0;">${date}</p>
         </div>
     `;
